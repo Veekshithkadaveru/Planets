8333c623b6d9ea6ba76c1e1118018f616f7d5789
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-21 16:52:42 +0530

    Navigate between pages

--- app/src/main/java/com/example/planets/MainActivity.kt
+++ app/src/main/java/com/example/planets/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.planets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.planets.model.getPlanets
+import com.example.planets.screen.DetailsView
+import com.example.planets.screen.HomePage
+import com.example.planets.screen.Screen
 import com.example.planets.ui.theme.PlanetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +20,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlanetsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                val navController = rememberNavController()
+                val planets = getPlanets()
+                var selectedPlanetInfo = remember {
+                    mutableStateOf(planets.first())
+                }
+                NavHost(navController = navController, startDestination = Screen.Home.route) {
+                    composable(route = Screen.Home.route) {
+                        HomePage(isVisible = true) { planetInfo ->
+                            selectedPlanetInfo.value = planetInfo
+                            navController.navigate(Screen.Details.route)
+                        }
+                    }
+                    composable(route = Screen.Details.route) {
+                        DetailsView(planetInfo = selectedPlanetInfo.value, isVisible = true) {
+                            navController.popBackStack()
+                        }
+                    }
                 }
             }
         }
